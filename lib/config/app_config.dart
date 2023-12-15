237b0a562cf0e40ffcdd756dcba697fa543d7ef3
ATuns <vip37atuan@gmail.com>
2023-12-15 11:45:40 +0700

    postLocation

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,6 +2,5 @@ class AppConfig {
   //ipconfig ipv4
   static const baseUrl = "http://${baseIp}:3000/";
   static const baseWs = "ws://${baseIp}:3000/";
-  static const baseIp = "172.16.16.165";
-
+  static const baseIp = "172.20.10.6";
 }
---
+++ lib/data/models/postLocation.dart
@@ -0,0 +1,35 @@
+import 'package:cow_students_connection/data/models/user.dart';
+
+class Coordinates {
+  final double latitude;
+  final double longitude;
+
+  Coordinates(this.latitude, this.longitude);
+
+  factory Coordinates.fromJson(Map<String, dynamic> json) {
+    return Coordinates(json['latitude'], json['longitude']);
+  }
+}
+
+class postLocation {
+  final String? id;
+  final user? owner;
+  final String? message;
+  final Coordinates? location; // Sử dụng lớp Coordinates để đại diện cho vị trí
+
+  postLocation(
+    this.id,
+    this.owner,
+    this.message,
+    this.location,
+  );
+
+  factory postLocation.fromJson(Map<String, dynamic> json) => postLocation(
+        json['_id'],
+        json['owner'] == null ? null : user.fromJson(json["owner"]),
+        json['message'],
+        json['location'] == null
+            ? null
+            : Coordinates.fromJson(json['location']),
+      );
+}
